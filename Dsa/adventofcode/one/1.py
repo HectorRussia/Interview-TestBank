844fe2b1bceaf059da9b1cd050e85faafc35d4bf
Ponkrit <ponkrit.wo@hotmail.com>
2025-12-02 16:06:53 +0700

    move new folder and fix variable name

--- Dsa/adventofcode/one/1.py
+++ Dsa/adventofcode/one/1.py
@@ -2,7 +2,7 @@
 from pathlib import Path
 
 
-p = Path("D:/TestFullstack/for_test_every/se-interviews/Dsa/adventofcode/my_input.txt")
+p = Path("D:/TestFullstack/for_test_every/se-interviews/Dsa/adventofcode/one/my_input.txt")
 
 with p.open("r",encoding="utf-8") as f:
     arr = [line.strip() for line in f if line.strip()]
@@ -19,13 +19,11 @@ for move in arr:
     direction = move[0]
     steps = int(move[1:])
 
-    if direction == "L":
-        position = (position + steps) % 100
-        count+1
-    elif direction == "R":
-        position = (position - steps) % 100
-        count+1
     if position == 0:
         count += 1
+    if direction == "L":
+        position = (position - steps) % 100
+    elif direction == "R":
+        position = (position + steps) % 100
 
 print(count)
